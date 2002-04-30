e1585047d6b0bbcc23b76f34b3967e670d83cc19
(no author) <(no author)@5922d1d1-9610-0410-a011-a6bce15726ed>
2002-04-30 14:49:24 +0000

    This commit was manufactured by cvs2svn to create tag 'V3_pre61'.

--- ccccdist/cccc/cccc_tbl.cc
+++ ccccdist/cccc/cccc_tbl.cc
@@ -45,10 +45,10 @@ template<class T>
 T* CCCC_Table<T>::find(string name)
 {
   T *retval=NULL;
-  iterator value_iterator=map_t::find(name);
+  map_t::iterator value_iterator=map_t::find(name);
   if(value_iterator!=end())
     {
-      retval=value_iterator->second;
+      retval=(*value_iterator).second;
     }
   return retval;
 }
@@ -105,7 +105,7 @@ template <class T> T* CCCC_Table<T>::next_item()
   T* retval=NULL;
   if(iter_!=end())
     {
-      retval=iter_->second;
+      retval=(*iter_).second;
       iter_++;
     }
   return retval;
